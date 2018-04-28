e14233852cb1554e994ebc4bc8ad5fc574050a0d
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2018-04-28 14:28:05 +0900

    🎉

--- lib/pry/octokit.rb
+++ lib/pry/octokit.rb
@@ -1,12 +1,23 @@
 require "pry/octokit/version"
 require 'pry'
 require 'octokit'
+require 'yaml'
 
-module Pry
+class Pry
   module Octokit
+    def self.client
+      @client ||= (
+        conf = YAML.load_file(File.expand_path("~/.config/hub"))
+        token = conf['github.com'].first['oauth_token']
+        ::Octokit::Client.new(access_token: token)
+      )
+    end
   end
 end
 
 Pry.commands.block_command(/^\/(.+)/) do |path|
-
+  path = eval %Q!"#{path}"!
+  resp = Pry::Octokit.client.get path
+  _pry_.show_result resp
+  _pry_.last_result = resp
 end
--- lib/pry/octokit/version.rb
+++ lib/pry/octokit/version.rb
@@ -1,4 +1,4 @@
-module Pry
+class Pry
   module Octokit
     VERSION = "0.1.0"
   end
