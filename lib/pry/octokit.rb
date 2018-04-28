6e457674caaf9453d6895d3dc873b7d21a679051
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2018-04-28 14:03:13 +0900

    bunlde gem

---
+++ lib/pry/octokit.rb
@@ -0,0 +1,7 @@
+require "pry/octokit/version"
+
+module Pry
+  module Octokit
+    # Your code goes here...
+  end
+end
---
+++ lib/pry/octokit/version.rb
@@ -0,0 +1,5 @@
+module Pry
+  module Octokit
+    VERSION = "0.1.0"
+  end
+end
